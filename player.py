a88b9fd4e5975bdea6beb2f5869edb8a3af19cf8
RafaelV1nueza <a01706351@tec.mx>
2023-10-09 19:10:39 -0600

    delta time correction, time between frames

--- player.py
+++ player.py
@@ -9,7 +9,12 @@ class Player:
         self.angle = PLAYER_ANGLE
 
     def movement(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx,dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
 
     def update(self):
         self.movement()
